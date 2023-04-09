50e92b793ba498701ba1b456d026691ea3f7a00f
dev <dev@SuFangdeMacBook-Pro.local>
2023-04-09 09:29:05 +0800

    Initial commit

---
+++ src/Test20230119.java
@@ -0,0 +1,12 @@
+public class Test20230119 {
+
+    public static void main(String [] args){
+        System.out.println("Hello Tsu!");
+        Test20230119 test = new Test20230119();
+        test.show();
+    }
+
+    public void show(){
+        System.out.println("hehe");
+    }
+}
